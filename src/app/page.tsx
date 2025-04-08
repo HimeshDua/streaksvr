6c2ec8f2629b119dcaf40096dd6bef354d30322c
HimeshDua <himeshdmaheswary@gmail.com>
2025-04-08 16:23:31 +0500

    selected theme ✔️

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,11 @@ import {HeroPage} from '@/components/HeroPage';
 export default function HomePage() {
   return (
     <div className="relative flex flex-col items-center justify-center min-h-screen overflow-hidden">
-      <div className="absolute inset-0 flex items-center justify-center opacity-100">
+      <div className="absolute inset-0 pointer-events-none -z-1 flex items-center justify-center opacity-100">
         <img
           alt="background"
           src="./square-alt-grid.svg"
-          className="object-cover w-full h-full opacity-60 dark:opacity-50 [mask-image:radial-gradient(75%_75%_at_center,white,transparent)]"
+          className="object-cover pointer-events-none -z-1 w-full h-full opacity-60 dark:opacity-50 [mask-image:radial-gradient(75%_75%_at_center,white,transparent)]"
         />
       </div>
       <HeroPage />
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -3,16 +3,16 @@ import SignupForm from '@/components/signup-form';
 function SignupPage() {
   return (
     <div className="flex flex-col items-center justify-center h-[calc(100vh-4rem)]">
-      <div className="absolute inset-0 flex items-center justify-center opacity-100">
+      <div className="absolute inset-0 pointer-events-none -z-1 flex items-center justify-center opacity-100">
         <img
           alt="background"
           src="./square-alt-grid.svg"
-          className="object-cover w-full h-full opacity-60 dark:opacity-50 [mask-image:radial-gradient(75%_75%_at_center,white,transparent)]"
+          className="object-cover pointer-events-none -z-1 w-full h-full opacity-60 dark:opacity-50 [mask-image:radial-gradient(75%_75%_at_center,white,transparent)]"
         />
       </div>
       <SignupForm className="w-full max-w-sm backdrop-blur-2xl" />
     </div>
   );
 }
-
+// streaksvr.vercel.app;
 export default SignupPage;
