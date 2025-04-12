1b4c1dce47140d2d7bce9cab04793d41403452a8
HimeshDua <himeshdmaheswary@gmail.com>
2025-04-12 16:39:22 +0500

    ui error fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
 import { HeroPage } from '@/components/HeroPage';
 
 export default function HomePage() {
-  return (
-    <div className="relative flex flex-col items-center justify-center min-h-screen overflow-hidden  bg-background/20 backdrop-blur-xs">
-      <HeroPage />
-    </div>
-  );
+  return <HeroPage />
 }
