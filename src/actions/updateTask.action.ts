5bc20fb7ea3c0f54260a963947d5d667853adfd2
HimeshDua <himeshdmaheswary@gmail.com>
2025-04-15 14:43:44 +0500

    badbar in inputs

--- src/actions/updateTask.action.ts
+++ src/actions/updateTask.action.ts
@@ -1,8 +1,31 @@
+'use server';
+import {prisma} from '@/lib/prisma';
+
 type TaskForm = {
   title: string;
   description: string;
-  category: string;
-  status: string;
+  category: 'WORK' | 'PERSONAL' | 'LEARNING' | 'OTHERS';
+  status: 'COMPLETED' | 'PENDING' | 'FAILED';
 };
 
-export default function updateTask(taskId: string, combinedData: any) {}
+export default async function updateTask(
+  taskId: string,
+  combinedData: TaskForm
+) {
+  try {
+    const updatedTask = await prisma.task.update({
+      where: {id: taskId},
+      data: {
+        title: combinedData.title,
+        description: combinedData.description,
+        category: combinedData.category,
+        status: combinedData.status
+      }
+    });
+
+    return updatedTask;
+  } catch (error) {
+    console.error('Error updating task:', error);
+    throw error;
+  }
+}
