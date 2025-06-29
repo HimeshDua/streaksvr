2223cf8bee799e3ba71eac6091cddc08378a4cdb
HimeshDua <himeshdua22@gmail.com>
2025-06-29 13:48:27 +0500

    github streaks error  test 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,22 @@
 'use client';
 
-import { useAuth } from '@/contexts/AuthContext';
+import {useAuth} from '@/contexts/AuthContext';
 import HomeTasksSection from '@/components/HomeTasks';
 import UnauthenticatedHomePage from '@/components/UnAuthenticatedHomePage';
 import ProfileInfo from '@/components/ProfileInfo';
 
-
 export default function HomePage() {
-  const { isAuthenticated, userData, streaks } = useAuth();
+  const {isAuthenticated, userData, streaks} = useAuth();
   if (!isAuthenticated) return <UnauthenticatedHomePage />;
 
+  const streakData =
+    Array.isArray(streaks) && streaks.length > 0 ? streaks[0] : {};
 
-  const streakData = Array.isArray(streaks) && streaks.length > 0 ? streaks[0] : {};
-
-
-
+  // chai pi lo friends
   return (
     <div className="min-h-screen bg-background py-0">
       <main className="px-4 sm:px-6 lg:px-8 py-8">
         <div className="max-w-5xl mx-auto space-y-6">
-
-
           <div className="text-center">
             <h1 className="text-5xl sm:text-6xl font-extrabold tracking-tight text-primary drop-shadow-md">
               Streaksvr
@@ -36,12 +32,14 @@ export default function HomePage() {
                     username: userData.username,
                     email: userData.email,
                     createdAt: userData.createdAt,
-                    tasksCount: userData.tasks?.length || 0,
+                    tasksCount: userData.tasks?.length || 0
                   }}
                   streak={{
                     current: Number(streakData?.current || 0),
                     longest: Number(streakData?.longest || 0),
-                    lastUpdated: streakData?.lastUpdated ? new Date(streakData.lastUpdated || "") : null,
+                    lastUpdated: streakData?.lastUpdated
+                      ? new Date(streakData.lastUpdated || '')
+                      : null
                   }}
                 />
               )}
